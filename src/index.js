0a236c50b16058c29bb56f1dddbdbb15b947341c
saluja1 <amit.saluja.106@gmail.com>
2020-12-11 15:15:20 +0530

    Basic React Todo APP

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Todo from './Todo.js';
+import TodoList from './TodoList.js';
+
+
+
+
+ReactDOM.render(
+    <Todo />,
+    document.getElementById('root')
+);
